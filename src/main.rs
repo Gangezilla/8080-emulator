a9c80bd88eebb820ef7f792006a4e45bb35bd90d
Scott <scottgangemi@gmail.com>
2019-06-10 11:32:45 +1000

    Bit more

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use std::env;
+use std::cell::RefCell;
 use std::fs::File;
 use std::io::Read;
-use std::cell::RefCell;
+use std::env;
 
+use std::rc::Rc;
 
 struct ConditionCodes {
-    z: u8,
-    s: u8,
-    p: u8,
-    cy: u8,
-    ac: u8,
+    z: u8,  // Zerp F;ag
+    s: u8,  // Sign Flag
+    p: u8,  // Parity Flag
+    cy: u8, // Carry Flag
+    ac: u8, // Auxilary Carry Flag
     pad: u8,
 }
 
@@ -22,14 +23,14 @@ pub struct State8080 {
     h: u8,
     l: u8,
     sp: u16,
-    pc: std::vec::Vec<std::cell::RefCell<u16>>, // this is very likely wrong...
-    memory: *mut u8,
+    pc: u16,
+    memory: Rc<RefCell<Memory>>,
     condition_codes: ConditionCodes,
     int_enable: u8,
 }
 
 impl State8080 {
-    pub fn new(mem: Vec<RefCell<u16>>) -> State8080 {
+    pub fn new(mem: Rc<RefCell<Memory>>) -> State8080 {
         State8080 {
             a: 0,
             b: 0,
@@ -39,8 +40,8 @@ impl State8080 {
             h: 0,
             l: 0,
             sp: 0,
-            pc: mem,
-            memory: std::ptr::null_mut(),
+            pc: 0,
+            memory: mem,
             condition_codes: ConditionCodes {
                 z: 0,
                 s: 0,
@@ -60,42 +61,78 @@ impl State8080 {
     pub fn set_c(&mut self, v: u8) {
         self.c = v;
     }
-}
 
-// fn unimplemented_instruction(instruction: &str) {
-//     eprintln!("Error: Unimplemented instruction ---- {:x}", instruction);
-//     std::process::exit(1);
-// }
-
-fn emulate_8080(state: &State8080, hex: u8) {
-    match &hex {
-        0x00 => (),
-        0x01 => {
-            state.set_c();
-            state.set_b();
-
-        }
-        _ => (),
+    pub fn get_current_pc(&mut self) -> u8 {
+        // called imm_ds???
+        let opcode = self.memory.borrow().get(self.pc);
+        self.pc += 1;
+        return opcode;
     }
 }
 
-// gets the opcode from state
-// how do i load our opcode into state. 
-//first hurdle yeah, really is getting my opcodes into state...
+pub struct Memory {
+    pub data: Vec<u8>,
+}
+
+impl Memory {
+    fn get(&self, a: u16) -> u8 {
+        self.data[usize::from(a)]
+    }
+
+    fn set(&mut self, a: u16, v: u8) {
+        self.data[usize::from(a)] = v
+    }
+
+    pub fn new() -> Self {
+        Self {
+            data: vec![0; 65536],
+        }
+    }
+}
+
+
+fn unimplemented_instruction(instruction: &u8) {
+    eprintln!("Error: Unimplemented instruction ---- {:x}", instruction);
+    std::process::exit(1);
+}
+
+// fn emulate_8080(state: &State8080) { // needs to return u32 later
+//     let opcode = state.get_current_pc();
+//     println!("{}", opcode);
+//     // match &hex {
+//     //     0x00 => (),
+//     //     0x01 => {
+//     //         state.set_c();
+//     //         state.set_b();
+//     //         state.increment_pc(2);
+//     //     }
+//     //     _ => (),
+//     // }
+// }
 
 fn main() {
     let filename = env::args().nth(1).expect("Please supply a filename");
+    let mem = Rc::new(RefCell::new(Memory::new()));
     let mut file = File::open(&filename).unwrap();
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).unwrap();
-    let mem = RefCell::new(&buffer[..]);
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).unwrap();
+    mem.borrow_mut().data[0x00..buf.len()].clone_from_slice(&buf[..]);
 
-    let mut state = State8080::new(mem);
-    // let mut position = 0;
-    // let end_position = buffer.len();
-    // while position < end_position {
-    //     let hex = &format!("{:x}", buffer[position]);
-    //     emulate_8080(&state, hex)
-    // }
-    // unimplemented_instruction();
+    let mut state = State8080::new(mem.clone());
+
+    loop {
+        let mut cycle = 0;
+        // implement cycles
+        // implement interrupts
+        // implement drawing pixels
+        // implement handling joypad
+
+
+        while cycle < 10 {
+            // cycle += emulate_8080(&state);
+            let opcode = state.get_current_pc();
+            println!("{}", 0xopcode);
+            cycle += 1;
+        }
+    }
 }
